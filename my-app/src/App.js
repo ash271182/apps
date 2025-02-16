81eefd6620bff270a6d88a395b633ba3186b9a26
ash271182 <ash271182@gmail.com>
2025-02-16 16:41:27 +0530

    new app

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Welcome to My App</h1>
+      <p>This is a simple UI deployed with Jenkins and Helm.</p>
+    </div>
+  );
+}
+
+export default App;
